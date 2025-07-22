8a2fd16a1d54be4a4bf04b0fcb2b02d134c6896f
Taprush091 <linkbossempire@gmail.com>
2025-07-22 20:51:15 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;nd8TER6K#xEt48oRoLD45OMWC99xxwINFvgm01BF3IXBwkJkT-kU", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;TdEBnKCS#yfwgfEkqVh4ipAo9MEQ2PhXDeOyx9z2wQfZPQXDnaNg", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
